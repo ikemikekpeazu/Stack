3a3b8d4ce81774cf458ee7f8bd3b3beb7471b54e
Ikem <iikekpeazu@gmail.com>
2026-04-15 10:34:05 -0400

    Initial Commit

---
+++ SavingsApp/SavingsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SavingsAppApp.swift
+//  SavingsApp
+//
+//  Created by Ikem Ikekpeazu on 4/15/26.
+//
+
+import SwiftUI
+
+@main
+struct SavingsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
